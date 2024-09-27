1270b16f1b6aee133b50cafdd49ba6821e9e98f8
Noob <101978891+ZxLbni@users.noreply.github.com>
2024-09-27 20:38:34 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "09814685583:AAH_nJs7-OKiFuHkYedfhrQV5LjkCB5g"; // Insert your bot token.
+const BOT_TOKEN = "7414077653:AAG9D52ilhyw7WZ0gAdp0wuzs9G-pXRK9kI"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
-const BOT_OWNER = 0064350805; // Insert your telegram account id.
-const BOT_CHANNEL = -10020358474134; // Insert your telegram channel id which the bot is admin in.
+const BOT_OWNER = 6742022802; // Insert your telegram account id.
+const BOT_CHANNEL = -1002180300815; // Insert your telegram channel id which the bot is admin in.
 const SIA_NUMBER = 1234; // Insert a random integer number and keep it safe.
 
 // ----------Ohh Bhai Do Not Modify ---------- // 
